7cf4e8cec131bc06eb7e269e13dfb527d10e2eb6
BuildTools <unconfigured@null.spigotmc.org>
2020-09-25 09:40:00 -0400

    updated plugins, updated paper, removed skript aliases

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-java -Xms4000M -Xmx7000M -jar paper-161.jar nogui
+java -Xms4000M -Xmx7000M -jar paper-204.jar nogui
 pause
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xms4G -Xmx5G -jar paper-161.jar -nogui
+java -Xms4G -Xmx5G -jar paper-204.jar -nogui
